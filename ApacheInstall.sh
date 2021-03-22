fe607bf28ededb7f80472a0ece11b075c7ce2a86
husainasad <shusain_be16@thapar.edu>
2021-03-23 01:25:44 +0530

    Installing Apache Server on EC2

--- ApacheInstall.sh
+++ ApacheInstall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # install httpd (Linux 2 version)
-yum update -y
-yum install -y httpd.x86_64
-systemctl start httpd.service
-systemctl enable httpd.service
+sudo su # Become super user
+yum update -y # Update instance
+yum install -y httpd.x86_64 # Install httpd
+systemctl start httpd.service # Start service
+systemctl enable httpd.service # Service remains enabled across reboot
+# Add HTTP Type in inbound rules before opening URL
 echo "Hello World from $(hostname -f)" > /var/www/html/index.html
